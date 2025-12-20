d0b0526c6d4aebe94274461d90a744ce9d39a6f5
Alper Seyman <alperseyman@gmail.com>
2025-12-20 14:17:45 +0100

    write tests and serializer for creat user API.

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='swagger-ui'),
+    path('api/user/', include('user.urls')),
 ]
