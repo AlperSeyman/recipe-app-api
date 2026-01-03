c2a0189a89c3c1c0393301f960b181bfaa86ee08
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-03 20:27:04 +0100

    added create_recipe func and public recipe api test

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -15,3 +15,32 @@ from core.models import Recipe
 
 from recipe.serializers import RecipeSerializer
 
+
+RECIPE_URL = reverse('recipe:recipe-list')
+
+def create_recipe(user, **kwargs):
+    """Create and return a sample recipe"""
+    default = {
+        'title':'Sample recipe title',
+        'time_minutes':22,
+        'price':Decimal('5.50'),
+        'description':'Sample description',
+        'link':'https://example.com/recipe.pdf'
+    }
+    default.update(kwargs)
+
+    recipe = Recipe.objects.create(user=user,**default)
+    return recipe
+
+class PublicRecipeAPITest(TestCase):
+    """Test unauthenticated API request."""
+
+    def setUp(self):
+        """Opens a new, empty browser window for the test to use"""
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required to call API."""
+        res = self.client.get(RECIPE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
