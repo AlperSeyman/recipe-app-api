bf6954113ee3e8f13e97ae41888f464d68d283b8
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-06 16:33:00 +0100

    added and implemented recipe listing API

---
+++ app/recipe/urls.py
@@ -0,0 +1,17 @@
+"""
+URL mappings for the user API
+"""
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from recipe.views import RecipeViewSet
+
+router = DefaultRouter()
+router.register('recipes', RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
