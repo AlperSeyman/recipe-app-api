424bffea3723dae49fab414d06bb447d51d9e88f
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-07 16:33:33 +0100

    added test_get_recipe_detail func and implemented

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -10,5 +10,11 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "description", "link"]
+        fields = ["id", "title", "time_minutes", "price", "link"]
         read_only_fields = ["id"]
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ["description"]
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -13,10 +13,14 @@ from rest_framework.test import APIClient
 
 from core.models import Recipe
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 RECIPE_URL = reverse('recipe:recipe-list')
+
+def detail_url(recipe_id):
+    """Create and return a recipe detail URL."""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 def create_recipe(user, **kwargs):
     """Create and return a sample recipe"""
@@ -90,3 +94,16 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertEqual(res.data, serializer.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_recipe_detail(self):
+        """Test viewing a recipe detail"""
+
+        recipe = create_recipe(user=self.user)
+
+        url = detail_url(recipe_id=recipe.id)
+
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+
+        self.assertEqual(res.data, serializer.data)
